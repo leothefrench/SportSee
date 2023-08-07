144d61353b5584054b44cbc6f79ef4ace0a09759
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-08-07 11:26:35 +0200

    Correction

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,12 +1,12 @@
-import USER_MAIN_DATA from '../../mock/data'
+// import USER_MAIN_DATA from '../../mock/data'
 import Header from './header/Header';
 
-const Dashboard = () => {
+// const Dashboard = () => {
 
-  const {firstName} = USER_MAIN_DATA;
+//   const {firstName} = USER_MAIN_DATA;
 
-  return (
-    <Header firstName={firstName} />
-  )
-}
-export default Dashboard
+//   return (
+//     <Header firstName={firstName} />
+//   )
+// }
+// export default Dashboard
