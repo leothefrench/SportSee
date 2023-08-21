54bb0bb09ce2cd9ffdff3468b1bc5f2a37fdbc31
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-08-21 10:28:09 +0200

    Implémentation barChart component & style done

--- src/components/dashboard/barChart/BarChartWeight.jsx
+++ src/components/dashboard/barChart/BarChartWeight.jsx
@@ -1,50 +1,50 @@
 import './barChartWeight.scss'
 import { USER_ACTIVITY } from '../../mock/data'
 import { useParams } from 'react-router-dom';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { ToolType } from './ToolType';
 import iconWeight from '../../../assets/barChart-icons/Oval-poids-icon.svg'
 import iconCaloriesBrulees from '../../../assets/barChart-icons/Oval-clories-brulees-icon.svg'
 
 const BarChartWeight = () => {
 
   const {userId} = useParams()
-  const data = USER_ACTIVITY.find((user) => user.id == userId)
+  const data = USER_ACTIVITY.find((user) => user.id === userId)
   
   if(!data) {
     return <div>Aucun utilisateur trouvé</div>
   }
 
-  let dataSessions = data.sessions.map((el) => {
-    return `${el.kilogram}`
+  const dataSessions = data.sessions.map((el, index) => {
+    return {
+      day: index + 1,
+      kilogram: el.kilogram,
+      calories: el.calories
+    }
   })
-
-  console.log(dataSessions)
-
-  for(let i =0; i < data.length; i++) {
-    data[i].day = i +1;
-  }
 
   return (
     <div className='legend'>
       <div className='legend__barChart'>
         <h1 className='legend__barChart__title'>Activité quotidienne</h1>
-        <Legend>
-          <div className='legend-poids'>
-            <span>{iconWeight}</span>
-            <span>Poids (kg)</span>
+        <div className='legend__barChart__icons'>
+          <div className='legend__barChart__icons__bullet'>
+            <img src={iconWeight} alt='weight' />
+            <span className='legend__barChart__icons__bullet__text'>Poids (kg)</span>
           </div>
-          <div className='legend-calories'>
-            <span>{iconCaloriesBrulees}</span>
-            <span>Calories brûlées (kCal)</span>
+          <div className='legend__barChart__icons__bullet'>
+            <img src={iconCaloriesBrulees} alt='calories' />
+            <span className='legend__barChart__icons__bullet__text'>Calories brûlées (kCal)</span>
           </div>
-        </Legend>
+        </div>
       </div>
       <ResponsiveContainer height={200}>
-        <BarChart data={data.sessions} barGap={8} barCategoryGap={1} >
+        <BarChart data={dataSessions} barGap={8} barCategoryGap={1} >
           <CartesianGrid strokeDasharray='1 1' vertical={false} />
           <XAxis dataKey='day' tickLine={false} tick={{fontSize: 14}} dy={15} stroke='1 1' />
           <YAxis yAxisId='kilogram' dataKey='kilogram' type='number' domain={['dataMin - 2', 'dataMax + 1']} tickCount="4" axisLine={false} orientation="right" tickLine={false} tick={{fontSize: 14}} dx={15} />
           <YAxis yAxisId='calories' dataKey='calories' type='number' domain={['dataMin - 20', 'dataMax + 10']} hide={true}  />
+          <Tooltip content={<ToolType />} />
           <Bar yAxisId="kilogram" dataKey="kilogram" fill="#282D30" barSize={7} radius={[50, 50, 0, 0]}/>
           <Bar yAxisId="calories" dataKey="calories" fill="#E60000" barSize={7} radius={[50, 50, 0, 0]}/>
         </BarChart>
